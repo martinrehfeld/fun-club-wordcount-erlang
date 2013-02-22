d04e9a9dbafb855848d2f35eb64ab6ffcfb5bd91
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2013-02-22 23:22:37 +0100

    a somewhat efficient lines iterator

--- src/wordcount.erl
+++ src/wordcount.erl
@@ -2,5 +2,23 @@
 
 -export([main/1]).
 
-main(_Args) ->
-    io:format("Hello World~n", []).
+-define(BUFSIZE, 1048576).
+-define(FILEMODE, [raw, read, {read_ahead, ?BUFSIZE}]).
+
+
+main(InputFileName) ->
+    {ok, Device} = file:open(InputFileName, ?FILEMODE),
+    each_line(Device, fun noop/2, nostate).
+
+each_line(Device, Fun, Acc) ->
+    case file:read_line(Device) of
+        {ok, Line} ->
+            NewAcc = Fun(Line, Acc),
+            each_line(Device, Fun, NewAcc);
+
+        eof  ->
+            Acc
+    end.
+
+noop(_Line, Acc) ->
+    Acc.
