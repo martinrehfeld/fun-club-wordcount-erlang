c028ab9e600165ad0f1a7fa76d2a812a5bcafa42
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2013-02-23 00:37:11 +0100

    single threaded: lists for line, dict for frequency
    real    0m0.941s
    user    0m0.880s
    sys     0m0.078s

--- src/wordcount.erl
+++ src/wordcount.erl
@@ -5,20 +5,39 @@
 -define(BUFSIZE, 1048576).
 -define(FILEMODE, [raw, read, {read_ahead, ?BUFSIZE}]).
 
+-define(SPACE, 32).
+-define(LOWERCASE_DELTA, 32).
 
-main(InputFileName) ->
-    {ok, Device} = file:open(InputFileName, ?FILEMODE),
-    each_line(Device, fun noop/2, nostate).
+main(FileName) ->
+    {ok, Io} = file:open(FileName, ?FILEMODE),
+    Freq = fold_lines(Io, fun map/1, fun reduce/2, dict:new()),
+    top(10, Freq).
 
-each_line(Device, Fun, Acc) ->
-    case file:read_line(Device) of
-        {ok, Line} ->
-            NewAcc = Fun(Line, Acc),
-            each_line(Device, Fun, NewAcc);
-
-        eof  ->
-            Acc
+fold_lines(Io, Map, Reduce, Acc) ->
+    case file:read_line(Io) of
+        {ok, Line} -> fold_lines(Io, Map, Reduce, Reduce(Map(Line), Acc));
+        eof        -> Acc
     end.
 
-noop(_Line, Acc) ->
-    Acc.
+map(Line) ->
+    string:tokens(sanitize(Line), " ").
+
+reduce(Words, Freq) ->
+    lists:foldl(fun (W, D) -> dict:update_counter(W, 1, D) end, Freq, Words).
+
+top(N, Freq) ->
+    L = [ {-C, {W, C}} || {W, C} <- dict:to_list(Freq) ],
+    Sorted = orddict:from_list(L),
+    do_top(N, Sorted).
+
+do_top(0, _)  -> ok;
+do_top(_, []) -> ok;
+do_top(N, [{_, {W, C}}|T]) -> io:format("~s: ~w~n", [W, C]), do_top(N-1, T).
+
+sanitize(Line) ->
+    lists:map(fun (C) when C <  $A -> ?SPACE;
+                  (C) when C =< $Z -> C + ?LOWERCASE_DELTA;
+                  (C) when C <  $a -> ?SPACE;
+                  (C) when C =< $z -> C;
+                  (_)              -> ?SPACE
+              end, Line).
