baa4a33e87c2bf55a952bb77e44d40eff8baae7d
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2013-02-23 03:12:59 +0100

    parallel: one process per line, shared ets for frequency
    real    0m0.672s
    user    0m1.362s
    sys     0m0.157s

--- src/wordcount.erl
+++ src/wordcount.erl
@@ -9,18 +9,40 @@
 -define(LOWERCASE_DELTA, 32).
 
 main(FileName) ->
-    {ok, Io} = file:open(FileName, ?FILEMODE),
-    Freq = fold_lines(Io, fun map/1, fun reduce/2, ets:new(freq, [])),
-    top(10, Freq).
+    Freq = ets:new(freq, [public, {write_concurrency, true}]),
+    Mgr = self(),
+    spawn_link(fun () ->
+                       %% one process is reading the file
+                       {ok, Io} = file:open(FileName, ?FILEMODE),
+                       fold_lines(Io, Mgr, fun map/4, fun reduce/2, Freq, 0)
+               end),
+    loop(Freq, 0, undefined).
 
-fold_lines(Io, Map, Reduce, Acc) ->
-    case file:read_line(Io) of
-        {ok, Line} -> fold_lines(Io, Map, Reduce, Reduce(Map(Line), Acc));
-        eof        -> Acc
+%% main process keeping track of progress
+loop(Freq, Done, Total) when Done =:= Total ->
+    top(10, Freq);
+loop(Freq, Done, Total) ->
+    receive
+        {total_lines, N} -> loop(Freq, Done    , N);
+        {mapper_done, _} -> loop(Freq, Done + 1, Total);
+        _                -> loop(Freq, Done    , Total)
     end.
 
-map(Line) ->
-    string:tokens(sanitize(Line), " ").
+fold_lines(Io, Mgr, Map, Reduce, Acc, Count) ->
+    case file:read_line(Io) of
+        {ok, Line} ->
+            spawn_link(fun () -> Map(Line, Reduce, Acc, Mgr) end),
+            fold_lines(Io, Mgr, Map, Reduce, Acc, Count+1);
+        eof ->
+            Mgr ! {total_lines, Count},
+            Acc
+    end.
+
+%% each line is mapped and reduced in a separate process, all update
+%% the shared state in ets
+map(Line, Reduce, Freq, Mgr) ->
+    Reduce(string:tokens(sanitize(Line), " "), Freq),
+    Mgr ! {mapper_done, self()}.
 
 reduce(Words, Freq) ->
     lists:foldl(fun (W, D) -> update_counter(W, D) end, Freq, Words).
