29b352bb9922bc46745ec71bedd2bfb863b3856d
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2013-02-24 01:44:34 +0100

    parallel: one process per 50 lines, frequency in ets
    real    0m0.513s
    user    0m0.928s
    sys     0m0.066s

--- src/wordcount.erl
+++ src/wordcount.erl
@@ -4,6 +4,7 @@
 
 -define(BUFSIZE, 1048576).
 -define(FILEMODE, [raw, read, {read_ahead, ?BUFSIZE}]).
+-define(BATCHSIZE, 50).
 
 -define(SPACE, 32).
 -define(LOWERCASE_DELTA, 32).
@@ -15,45 +16,53 @@ main(FileName) ->
                        %% one process is reading the file
                        {ok, Io} = file:open(FileName, ?FILEMODE),
                        Process =
-                           fun (Line) ->
-                                   %% each line is mapped and reduced in a
-                                   %% separate process, all update the shared
-                                   %% state in ets
+                           fun (Lines) ->
+                                   %% each batch is processed in a separate
+                                   %% process, all update shared state in ets
                                    spawn_link(fun () ->
-                                           process(Line, Freq),
-                                           Mgr ! mapper_done
+                                           lists:foreach(fun (L) ->
+                                                                 process(L, Freq)
+                                                         end, Lines),
+                                           Mgr ! batch_done
                                    end)
                            end,
-                       LineCount = foreach_lines(Io, Process),
-                       Mgr ! {total_lines, LineCount}
+                       BatchCount = foreach_lines(Io, ?BATCHSIZE, Process),
+                       Mgr ! {total_batches, BatchCount}
                end),
     loop(Freq, 0, undefined).
+
 
 %% main process keeping track of progress
 loop(Freq, Done, Total) when Done =:= Total ->
     top(10, Freq);
 loop(Freq, Done, Total) ->
     receive
-        {total_lines, N} -> loop(Freq, Done    , N);
-        mapper_done      -> loop(Freq, Done + 1, Total);
-        _                -> loop(Freq, Done    , Total)
+        {total_batches, N} -> loop(Freq, Done    , N);
+        batch_done         -> loop(Freq, Done + 1, Total);
+        _                  -> loop(Freq, Done    , Total)
     end.
 
-foreach_lines(Io, Fun) ->
-    foreach_lines(Io, Fun, 0).
 
-foreach_lines(Io, Process, Count) ->
+foreach_lines(Io, Batchsize, Fun) ->
+    foreach_lines(Io, 0, Batchsize, [], Fun, 0).
+
+foreach_lines(Io, Batchsize, Batchsize, Acc, Fun, Count) ->
+    Fun(Acc),
+    foreach_lines(Io, 0, Batchsize, [], Fun, Count+1);
+foreach_lines(Io, N, Batchsize, Acc, Fun, Count) ->
     case file:read_line(Io) of
         {ok, Line} ->
-            Process(Line),
-            foreach_lines(Io, Process, Count+1);
+            foreach_lines(Io, N+1, Batchsize, [Line|Acc], Fun, Count);
         eof ->
-            Count
+            Fun(Acc),
+            Count+1
     end.
+
 
 process(Line, Freq) ->
     Words = string:tokens(sanitize(Line), " "),
     lists:foreach(fun (W) -> update_counter(W, Freq) end, Words).
+
 
 top(N, Freq) ->
     L = [ {-C, {W, C}} || [{W, C}] <- ets:match(Freq, '$1') ],
@@ -63,6 +72,7 @@ top(N, Freq) ->
 do_top(0, _)  -> ok;
 do_top(_, []) -> ok;
 do_top(N, [{_, {W, C}}|T]) -> io:format("~s: ~w~n", [W, C]), do_top(N-1, T).
+
 
 sanitize(Line) ->
     lists:map(fun (C) when C <  $A -> ?SPACE;
